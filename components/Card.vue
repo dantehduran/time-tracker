7d50a81596045e5fd12b920636ff638e4aa2dc9d
dante <dantehduran@gmail.com>
2022-07-19 16:50:53 -0500

    control time component style

--- components/Card.vue
+++ components/Card.vue
@@ -3,10 +3,12 @@
 		<div class="h-12 text-gray-600 opacity-50 flex justify-end pr-4">
 			<Icon :icon="icon" width="50" height="50" class="rotate-45" />
 		</div>
-		<div class="p-6 bg-indigo-900 rounded-xl flex flex-col">
+		<div class="p-6 bg-indigo-900 rounded-xl flex flex-col relative">
+			<ControlTime v-if="timeframe === 'day'" class="top-4 right-4 absolute" />
 			<div class="flex justify-between items-center">
 				<h4 class="capitalize text-white text-2xl">{{ title }}</h4>
-				<span class="text-white font-bold text-2xl">...</span>
+
+				<!-- <span v-else class="text-white font-bold text-2xl">...</span> -->
 			</div>
 			<div class="mt-6 mb-2">
 				<h3 class="text-white text-5xl tracking-wide mb-2">
---
+++ components/ControlTime.vue
@@ -0,0 +1,17 @@
+<template>
+	<button
+		class="rounded-full h-14 w-14 bg-red-500 flex items-center justify-center"
+		@click="active = !active"
+	>
+		<Icon
+			:icon="active ? 'bi:pause-fill' : 'bi:play-fill'"
+			width="48"
+			height="48"
+			class="text-white"
+		/>
+	</button>
+</template>
+<script setup lang="ts">
+import { Icon } from '@iconify/vue';
+const active = ref(false);
+</script>
