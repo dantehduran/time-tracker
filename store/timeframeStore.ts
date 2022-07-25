2cdd9e9abb3f3c098f1706d680208f9293b062c6
dante <dantehduran@gmail.com>
2022-07-25 10:10:59 -0500

    pinia hmr

--- store/timeframeStore.ts
+++ store/timeframeStore.ts
@@ -149,3 +149,6 @@ export const useTimeframe = defineStore('timeframe', {
 	},
 	// persist: true,
 });
+
+if (import.meta.hot)
+  import.meta.hot.accept(acceptHMRUpdate(useUserStore, import.meta.hot))
