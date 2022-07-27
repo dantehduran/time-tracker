f227605a0e13e7a59135065983974e9e75dcef6f
dante <dantehduran@gmail.com>
2022-07-27 18:45:56 -0500

    cookie storage expires max 2 months

--- plugins/PiniaPlugin.ts
+++ plugins/PiniaPlugin.ts
@@ -2,5 +2,11 @@ import { createNuxtPersistedState } from 'pinia-plugin-persistedstate';
 import { defineNuxtPlugin, useCookie } from '#app';
 
 export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.$pinia.use(createNuxtPersistedState(useCookie));
+	nuxtApp.$pinia.use(
+		createNuxtPersistedState(useCookie, {
+			cookieOptions: {
+				maxAge: 60 * 60 * 24 * 31 * 2,
+			},
+		})
+	);
 });
