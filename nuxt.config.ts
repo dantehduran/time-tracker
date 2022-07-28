cb629a70668aa6448a1ab342014fdd80766eaa38
dante <dantehduran@gmail.com>
2022-07-27 19:15:29 -0500

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,14 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			link: [
+				{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+				{ rel: 'icon', type: 'image/png', href: '/favicon.png' },
+			],
+		},
+	},
 	build: {
 		postcss: {
 			postcssOptions: {
