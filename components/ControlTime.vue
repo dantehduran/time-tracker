4b13662dd7eaefe3638a2cc725b06aa90c4bc6fb
dante <82616105+dantehduran@users.noreply.github.com>
2023-02-11 14:48:41 -0500

    minutes to seconds

--- components/ControlTime.vue
+++ components/ControlTime.vue
@@ -1,14 +1,6 @@
 <template>
-	<button
-		class="rounded-full h-14 w-14 bg-red-500 flex items-center justify-center"
-		@click="handleTime"
-	>
-		<Icon
-			:icon="active ? 'bi:pause-fill' : 'bi:play-fill'"
-			width="48"
-			height="48"
-			class="text-white"
-		/>
+	<button class="rounded-full h-14 w-14 bg-red-500 flex items-center justify-center" @click="handleTime">
+		<Icon :icon="active ? 'bi:pause-fill' : 'bi:play-fill'" width="48" height="48" class="text-white" />
 	</button>
 </template>
 <script setup lang="ts">
@@ -18,7 +10,7 @@ const store = useTimeframe();
 const props = defineProps<{
 	activity: string;
 }>();
-const { counter, pause, resume } = useInterval(60000, { controls: true });
+const { counter, pause, resume } = useInterval(1000, { controls: true });
 const active = ref(false);
 onMounted(() => pause());
 const emit = defineEmits(['timeChange']);
